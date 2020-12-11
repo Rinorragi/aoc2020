e8ac3c1020e89cf78f0f2c33084817bb966604e4
Rinorragi <rinorragi@gmail.com>
2020-12-11 09:46:23 +0200

    Day 10 - part 2 some advancement but not finished

--- src/Day10/Program.fs
+++ src/Day10/Program.fs
@@ -5,6 +5,9 @@ type JoltageAdapter = {
     Increment : int
     Value : int}
 
+let getThrd (_, _, c, _) = c
+let getSnd (_, b, _, _) = b
+let getFrth (_, _, _, d) = d
 
 let constructJoltageAdapterChain (input : int array) =
     let joltageChain : JoltageAdapter list = []
@@ -46,18 +49,56 @@ let calculatePart1Answer (differences : (int * int) list) =
         |> single (fun f -> (fst f) = 3)
     (snd ones) * (snd threes)
 
+let joltageAdaptersCouldBeDropped (joltageAdapterChain : JoltageAdapter list) =
+
+    joltageAdapterChain 
+    |> List.mapi (fun i f -> 
+        let prevValue = if (i = 0) then 0 else joltageAdapterChain.[i - 1].Value
+        let canBeDroppedAlone = 
+            i < joltageAdapterChain.Length - 1 
+            && joltageAdapterChain.[i+1].Value <= prevValue + 3
+        let canbeDoubleDroppedWithNextOne =
+            canBeDroppedAlone
+            && i < joltageAdapterChain.Length - 2
+            && joltageAdapterChain.[i+2].Value <= prevValue + 3 
+        (f, i, (canBeDroppedAlone), canbeDoubleDroppedWithNextOne))
+
+let factorial (n : bigint) = [(bigint 1)..n] |> List.reduce (*)
+
 [<EntryPoint>]
 let main argv =
     printfn "Advent of Code Day 10 - Part 1"
 
     let joltInput = 
-        File.ReadAllLines("./input/input_day10.txt")
+        File.ReadAllLines("./input/input_day10_example2.txt")
         |> Array.map (fun f -> f |> System.Int32.Parse)
 
-    let joltageAdapterChain = constructJoltageAdapterChain joltInput
+    let joltageAdapterChain = 
+        constructJoltageAdapterChain joltInput
+        |> List.rev
     let joltageAdapterCounts = calculateJoltageAdapters joltageAdapterChain
 
     let answer1 = calculatePart1Answer joltageAdapterCounts
 
     printfn "With joltage adapters: %A the answer is : %d " joltageAdapterCounts answer1
+    printfn "Advent of Code Day 10 - Part 2"
+    let couldBeDroppedAdapters = 
+        joltageAdaptersCouldBeDropped joltageAdapterChain
+        |> List.filter (getThrd)
+        |> List.map (getSnd)
+
+    let couldBeDoubleDroppedAdapters =
+        joltageAdaptersCouldBeDropped joltageAdapterChain
+        |> List.filter (getFrth)
+        |> List.map (getSnd)
+
+    let singleCount = couldBeDroppedAdapters |> Seq.length
+    let doubleCount = couldBeDoubleDroppedAdapters |> Seq.length
+    printfn "Single droppable %d double droppable %d" singleCount doubleCount 
+
+    let singleCountPermutations = factorial (bigint singleCount)
+    let doubleCountPermutations = factorial (bigint doubleCount)
+    printfn "%d + %A + %A" 1 singleCountPermutations doubleCountPermutations
+    // TODO: figure out how to calculate permutations without brute forcing since trying to iterate 15! with example values kills the CPU
+
     0 // return an integer exit code
