b0c65a216d3199b19e707838303ca2f6ae6f61ff
Joona Immonen <joona.immonen@solita.fi>
2020-12-15 08:55:17 +0200

    Day 15 - part 2

--- src/Day15/Program.fs
+++ src/Day15/Program.fs
@@ -1,4 +1,5 @@
 open System
+open System.Collections.Generic
 
 [<EntryPoint>]
 let main argv =
@@ -6,20 +7,19 @@ let main argv =
     // Set last number to last number spot
     let mutable lastNumber = initialNumbers.[initialNumbers.Length - 1]
     // Initialize so that the last number is still missing from the list
-    let mutable (spokenNumbers : int list) = initialNumbers.[0 .. initialNumbers.Length - 2]
+    let spokenNumbersDict = new Dictionary<int, int>()
+    for i in [0 .. initialNumbers.Length - 2] do
+        spokenNumbersDict.Add(initialNumbers.[i], i)
     // 0-based array so 2019 is the 2020th
     for i in [initialNumbers.Length .. 29999999] do
         let lastRoundNumber = lastNumber
-        if (spokenNumbers |> List.exists (fun f -> f = lastRoundNumber))
+        if spokenNumbersDict.ContainsKey lastRoundNumber
         then
-            let lastIndex = (spokenNumbers |> List.findIndexBack (fun f -> f = lastRoundNumber))
-            lastNumber <- spokenNumbers.Length - lastIndex
+            let lastIndex = spokenNumbersDict.[lastRoundNumber]
+            lastNumber <- i - lastIndex - 1
         else 
             lastNumber <- 0
-        spokenNumbers <- [lastRoundNumber] |> List.append spokenNumbers
+        spokenNumbersDict.[lastRoundNumber] <- i - 1
         if i = 2019 then printfn "Part 1 answer: %d" lastNumber
-        if i % 100000 = 0 then printfn "%d %A" i ((System.DateTime.Now).ToLocalTime())
     printfn "Part 2 answer: %d" lastNumber
-        
-
     0 // return an integer exit code
