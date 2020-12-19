2f3f5ad24c2d5db1aa7d14552801811711eeaafe
Rinorragi <rinorragi@gmail.com>
2020-12-19 08:10:39 +0200

    Day 18 - dirty but part 2 works now. Let's see later if I will tidy this up

--- src/Day18/Program.fs
+++ src/Day18/Program.fs
@@ -44,22 +44,24 @@ let rec findClosingBracket (mathStr : string) (index : int) (acc : int) =
     | _ -> findClosingBracket (mathStr.[1 ..]) (index + 1) acc
 
 let rec findContinuousInteger (mathStr : string) (index : int) =
+    //printfn "FCI: '%s' %d" mathStr index
     if mathStr.Length = 0
     then 
-        index + 1
+        index - 1
     else 
-        match (string mathStr.[0]) with
+        match (string (mathStr |> Seq.head)) with
         | Int64Matcher _ -> findContinuousInteger (mathStr.[1 ..]) (index + 1)
-        | _ -> index
+        | _ -> index - 1
 
 let rec findReverseContinuousInteger (mathStr : string) (index : int) =
-    if index = 0
+    //printfn "FRCI: '%s' %d" mathStr index
+    if mathStr.Length = 0
     then
-        index
+        index + 1
     else
-        match (string mathStr.[mathStr.Length - 1]) with
-        | Int64Matcher _ -> findContinuousInteger (mathStr.[.. mathStr.Length - 2]) (index - 1)
-        | _ -> index
+        match (string (mathStr |> Seq.last)) with
+        | Int64Matcher _ -> findReverseContinuousInteger (mathStr.[.. mathStr.Length - 2]) (index - 1)
+        | _ -> index + 1
 
 let rec solveString (mathStr : string) (plusBeforeMultiply : bool) (printSteps : bool) =
     if printSteps
@@ -73,31 +75,33 @@ let rec solveString (mathStr : string) (plusBeforeMultiply : bool) (printSteps :
             calculateNext (mathStr.Split(" ",StringSplitOptions.RemoveEmptyEntries)) "" (int64 0)
         else
             let plusIndex = mathStr.IndexOf("+")
-            let firstString = mathStr.[.. plusIndex - 2]
-            let lastString = mathStr.[plusIndex + 2 ..]
-            let startIndex = findReverseContinuousInteger firstString (plusIndex - 3)
-            let endIndex = findReverseContinuousInteger lastString (plusIndex + 2)
+            let firstHalf = mathStr.[.. plusIndex - 2]
+            let secondhalf= mathStr.[plusIndex + 2 ..]
+            let startIndex = findReverseContinuousInteger firstHalf (plusIndex - 2)
+            let endIndex = findContinuousInteger secondhalf (plusIndex + 2)
+            
             let subString = mathStr.[startIndex .. endIndex]
             if printSteps
             then 
+                printfn "Pre-sub: '%s' '%c' '%c'" mathStr mathStr.[startIndex] mathStr.[endIndex]
                 printfn "PTS-sub: '%s' %d %d" subString startIndex endIndex
             let subValue = solveString subString plusBeforeMultiply printSteps
             
-            let firstStr =
+            let startStr =
                 if startIndex = 0
                 then ""
-                else mathStr.[.. startIndex]
-            let lastStr = 
+                else mathStr.[.. startIndex - 1]
+            let endStr = 
                 if (endIndex + 1 >= mathStr.Length) 
                 then ""
                 else mathStr.[endIndex + 1 .. ]
             if printSteps
             then     
-                printfn "PTS: %s '+' %s || '%s''%d''%s'" firstString lastString firstStr subValue lastStr
+                printfn "PTS: %s '+' %s || '%s''%d''%s'" firstHalf secondhalf startStr subValue endStr
             let prettifiedMathStr = (
-                firstStr
+                startStr
                 + subValue.ToString("G").ToLowerInvariant() 
-                + lastStr)
+                + endStr)
             solveString prettifiedMathStr plusBeforeMultiply printSteps
     else
         let firstIndex = mathStr.IndexOf("(")
@@ -117,20 +121,18 @@ let rec solveString (mathStr : string) (plusBeforeMultiply : bool) (printSteps :
 [<EntryPoint>]
 let main argv =
     printfn "Advent of Code Day 18"
-    let mathInput = System.IO.File.ReadAllLines "./input/input_day18_example.txt"
-    (*
+    let mathInput = System.IO.File.ReadAllLines "./input/input_day18.txt"
+
     let answer1 = 
         mathInput
-        |> Array.sumBy (fun s -> solveString s false)
+        |> Array.sumBy (fun s -> solveString s false false)
     printfn "Part 1 answer: %d" answer1
-    *)
+
     let answer2 = 
         mathInput
-        //|> Array.skip 5
         |> Array.sumBy (fun s -> 
             let a = solveString s true false
-            printfn "Answer for %s: %d" s a
+            //printfn "Answer for %s: %d" s a
             a)
-        //|> Array.sumBy (fun s -> solveString s true)
     printfn "Part 2 answer: %d" answer2
     0 // return an integer exit code
